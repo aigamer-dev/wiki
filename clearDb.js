f94618adff3dc0e56d07ac44b2aa5156acfcda1f
Hariharan <hariharan@aigamer.dev>
2026-03-04 16:05:10 +0530

    security: remove hardcoded firebase API key from scripts

--- clearDb.js
+++ clearDb.js
@@ -1,12 +1,13 @@
+import "dotenv/config";
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, deleteDoc, doc } from "firebase/firestore";
 
 const firebaseConfig = {
-    projectId: "aigamer-encyclopedia-v3",
-    appId: "1:363809660823:web:56c9cde0bc71196b646ed2",
-    storageBucket: "aigamer-encyclopedia-v3.firebasestorage.app",
-    apiKey: "AIzaSyB3M4C4_6jQfwu7cepg6lYVsjC6FnbaORc",
-    authDomain: "aigamer-encyclopedia-v3.firebaseapp.com",
+    projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+    appId: process.env.VITE_FIREBASE_APP_ID,
+    storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+    apiKey: process.env.VITE_FIREBASE_API_KEY,
+    authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
 };
 
 const app = initializeApp(firebaseConfig);
